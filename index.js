fb27047ad8addb3ff74820a75115581fe2ac4239
Carlos Ramirez <carlosramirez686@gmail.com>
2022-01-08 17:09:17 -0800

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+    let find = record.find(finder => finder.result === "W")
+    if(!!find){
+        return find.year
+    }
+    else {
+        return undefined
+    }
+    //console.log(find)
+
+   
+}
